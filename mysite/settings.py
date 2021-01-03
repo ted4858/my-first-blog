16ed4665c670f399660af307a1728302f6e026b3
daewung so <daiwooung4858@gmail.com>
2021-01-03 19:46:37 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_DIRS = {
+    os.path.join(BASE_DIR, 'static'),
+    }
+
 LOGIN_REDIRECT_URL = '/'
